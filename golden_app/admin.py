81815b4647cc1302b32c2cdb1dad0097044c6d1f
Shajahan Palayil <shajahan.pts@gmail.com>
2012-07-29 15:08:27 +0200

    Inital skeleton

---
+++ golden_app/admin.py
@@ -0,0 +1,5 @@
+from golden_app.models import Product
+from golden_app.models import Addon
+from django.contrib import admin
+admin.site.register(Product)
+admin.site.register(Addon)
---
+++ golden_app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+class Addon(models.Model):
+    name = models.CharField(max_length = 100)
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
+
+    def _unicode_(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length = 50)
+    base_price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    short_description = models.CharField(max_length = 100)
+    long_description = models.CharField(max_length = 1000)
+    thumbnail = models.ImageField(upload_to = 'thumbnails')
+    product_image = models.ImageField(upload_to = 'images')
+    addons = models.ManyToManyField(Addon)
+
+    def _unicode_(self):
+        return name
+
+class User(models.Model):
+    email_id = models.EmailField(max_length = 75)
+
+    def _unicode_(self):
+        return email_id
+
+
+class Item(models.Model):
+    delivery_address = models.CharField(max_length = 1000)
+    price = models.DecimalField(max_digits = 7, decimal_places = 2)
+    user = models.OneToOneField(User)
+    product = models.OneToOneField(Product)
+
+    def _unicode_(self):
+        return product.name
+
